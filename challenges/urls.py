f62a6ba5d5bbaaf211c8d3b527dfbda835728cb2
Utsav Adhikari <utsavadhikari452@gmail.com>
2023-05-01 16:06:42 +0545

    this is the third commit

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 # then import the views from your views.py that are the functions or classes that are to be sent when the client request it 
 ## then import the views from your views.py that are the functions or classes 
+## then import the views from your views.py that are the functions or classes 
 from . import views
 
 urlpatterns =[
